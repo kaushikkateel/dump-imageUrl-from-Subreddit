84d4c17d58d2698e2ab7dddb02a1d858049660aa
Kaushik <49521970+kaushikkateel@users.noreply.github.com>
2020-06-22 22:02:32 +0530

    Add files via upload

---
+++ reddit-image-urls.py
@@ -0,0 +1,20 @@
+import praw
+
+#get client information from reddit 
+r = praw.Reddit(client_id='',
+				client_secret= '',
+				username= '',
+				password= '',
+				user_agent='')
+
+#subreddit name
+subR = r.subreddit('aww')
+
+#choose sort type
+hot = subR.hot(limit = 200) 
+f = open("urls.txt", "a")
+for post in hot:
+	f.write(post.url)
+	f.write("\n")
+
+f.close()
